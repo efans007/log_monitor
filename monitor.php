3bd25b849c7aa47e27a83ef8c5823f33644395a4
lingsiyong <lingsiyong@beiquan.org>
2015-12-08 14:53:31 +0800

    update usage

--- monitor.php
+++ monitor.php
@@ -2,8 +2,8 @@
 
 // usage:
 //
-// create crontab:
-// * * * * * flock -nx /tmp/script.logMonitor6.lock -c '/usr/bin/php5 /path/to/monitor.php /var/log/beiquan.log >> logMonitor.log'
+// add crontab:
+// * * * * * flock -nx /tmp/script.logMonitor.lock -c '/usr/bin/php5 /path/to/monitor.php /var/log/beiquan.log >> logMonitor.log'
 
 require __DIR__ . '/vendor/autoload.php';
 
